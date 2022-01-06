d64f373556a6541c41bedd2ddd9630a7eebd9556
JulenC <julencastillo98@gmail.com>
2022-01-06 13:25:07 +0100

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/full-demo/' }),
+    res.sendFile('index.html', { root: 'dist/resilence-tech/' }),
 );
 
 // Start the app by listening on the default Heroku port
